68b7b283dddc7c4e7a5c7a55bddb7bd6e6470b39
MrSuricate <kevindu032270@gmail.com>
2025-02-27 15:47:56 +0100

    remove console log process.env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-console.log(process.env)
-
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
